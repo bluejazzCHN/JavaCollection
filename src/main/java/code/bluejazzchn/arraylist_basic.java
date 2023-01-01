ce8e9826d52e337a2003183c4d731de7c610dc5a
OldCodeFans <songjiangzhang@hotmail.com>
2023-01-01 17:43:24 +0800

    init

--- src/main/java/code/bluejazzchn/arraylist_basic.java
+++ src/main/java/code/bluejazzchn/arraylist_basic.java
@@ -10,14 +10,17 @@ import java.util.ArrayList;
 
 public class arraylist_basic {
     public static void main(String[] args) {
+
+        //创建学生姓名 ArrayList
         ArrayList studentNameList = new ArrayList();
         studentNameList.add("小王");
         studentNameList.add("小张");
         studentNameList.add("小李");
 
-        System.out.println("学生列表：");
-        for (Object stuName : studentNameList){
-            System.out.println(stuName);
+        System.out.println("学生列表，共{" + studentNameList.size() + "人}:");
+        //遍历学生姓名
+        for (Object s : studentNameList) {
+            System.out.println(s);
         }
     }
 }
---
+++ src/main/java/code/bluejazzchn/arraylist_generic.java
@@ -0,0 +1,41 @@
+/*
+ * 名称：ArrayList泛型使用演示
+ * 作者：bluejazzchn
+ *
+ * */
+package code.bluejazzchn;
+
+import java.util.ArrayList;
+
+class Student {
+    public String name;
+    public float grd;
+
+    Student(String n, float g) {
+        this.name = n;
+        this.grd = g;
+    }
+}
+
+public class arraylist_generic {
+    public static void main(String[] args) {
+        //创建3个学生的数据结构
+        Student wang = new Student("小王", 2.5f);
+        Student zhang = new Student("小张", 3.5f);
+        Student li = new Student("小李", 4.5f);
+
+        //创建泛型的Arraylist，并添加上面创建的三个学生信息
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(wang);
+        studentList.add(zhang);
+        studentList.add(li);
+
+        //便利学生信息
+        System.out.println("学生列表，共{" + studentList.size() + "人}:");
+        for (Student s : studentList) {
+            System.out.println("-------------------------");
+            System.out.print("学生姓名：" + s.name);
+            System.out.println("  等级分：" + s.grd);
+        }
+    }
+}
