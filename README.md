d4fc3868cc3ae04da733d61c46cbe165f2da0b99
OldCodeFans <songjiangzhang@hotmail.com>
2023-01-01 19:55:01 +0800

    add hashmap_basic.java

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JavaCollection
 Demonstrates the basic use of the Java Collection through an example.
+
+- ArrayList demo:
+  - arraylist_basic.java
+  - arraylist_generic.java
+- HashMap demo: 
---
+++ src/main/java/code/bluejazzchn/hashmap/hashmap_basic.java
@@ -0,0 +1,33 @@
+/*
+ * 名称：HashMap基本使用演示
+ * 作者：bluejazzchn
+ *
+ * */
+package code.bluejazzchn.hashmap;
+
+import java.util.HashMap;
+
+public class hashmap_basic {
+    public static void main(String[] args) {
+        //声明保存Key：学生学号Integer和Value：姓名String的HashMap
+        HashMap<Integer, String> studentHashMap = new HashMap<>();
+        studentHashMap.put(102, "小王");
+        studentHashMap.put(103, "小张");
+        studentHashMap.put(104, "小李");
+
+        //输出HashMap的结果 {102=小王, 103=小张, 104=小李}
+        System.out.println("-------------------------");
+        System.out.println(studentHashMap);
+
+        //声明保存Key：姓名String和Value：学生学号Integer的HashMap
+        HashMap<String, Integer> studentHashMap1 = new HashMap<>();
+        studentHashMap1.put("小王", 102);
+        studentHashMap1.put("小张", 103);
+        studentHashMap1.put("小李", 104);
+
+        //输出HashMap的结果 {小李=104, 小王=102, 小张=103}
+        System.out.println("-------------------------");
+        System.out.println(studentHashMap1);
+
+    }
+}
