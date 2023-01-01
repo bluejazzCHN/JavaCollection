5ba614106eb0988cef9ce992cc28c6420c749858
OldCodeFans <songjiangzhang@hotmail.com>
2023-01-01 19:08:18 +0800

    use iterator to access arraylist_basic

--- src/main/java/code/bluejazzchn/arraylist/arraylist_basic.java
+++ src/main/java/code/bluejazzchn/arraylist/arraylist_basic.java
@@ -7,6 +7,7 @@
 package code.bluejazzchn.arraylist;
 
 import java.util.ArrayList;
+import java.util.*;
 
 public class arraylist_basic {
     public static void main(String[] args) {
@@ -18,9 +19,16 @@ public class arraylist_basic {
         studentNameList.add("小李");
 
         System.out.println("学生列表，共{" + studentNameList.size() + "人}:");
-        //遍历学生姓名
+        //使用for循环遍历学生姓名
+        System.out.println("-------------------------");
         for (Object s : studentNameList) {
             System.out.println(s);
+        }
+        //使用迭代器循环遍历学生姓名
+        Iterator it = studentNameList.iterator();
+        System.out.println("-------------------------");
+        while (it.hasNext()) {
+            System.out.println(it.next());
         }
     }
 }
